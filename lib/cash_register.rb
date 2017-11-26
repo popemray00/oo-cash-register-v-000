19e0391befe49bc08ab38e788eca0bda987c0abc
Michael Pope <popemray1986@gmail.com>
2017-11-26 18:12:36 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
+class CashRegister
+  attr_accessor :new, :cash_register, :total
 
+
+  def initialize
+    @total = self.total
+    self.total = 0
+  end
+
+  def add_item(title, price)
+    self.total += price
+
+
+
+
+
+  end
+
+
+
+
+
+
+
+
+end
